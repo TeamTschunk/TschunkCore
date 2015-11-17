a12618154731bb19c3d55deda357b0ec7214eeac
PeterTheOne <petertheone@gmail.com>
2015-11-17 15:21:31 +0100

    capture and scan image (results not used)

--- tschunk/vision.py
+++ tschunk/vision.py
@@ -6,6 +6,38 @@
 # 5 ... Drop
 # 6 ... Start
 
+import zbar
+from PIL import Image
+from SimpleCV import Image as SimpleImage, Camera
+
 class vision(object):
+
+    cam = Camera(2)
+
     def get_commands_array (self):
+        img = self.cam.getImage()
+        img.save('image.jpg')
+
+        # create a reader
+        scanner = zbar.ImageScanner()
+
+        # configure the reader
+        scanner.parse_config('enable')
+
+        # obtain image data
+        pil = img.getPIL().convert('L')
+        width, height = pil.size
+        raw = pil.tostring()
+
+        # wrap image data
+        image = zbar.Image(width, height, 'Y800', raw)
+
+        # scan the image for barcodes
+        scanner.scan(image)
+
+        # extract results
+        for symbol in image:
+            # do something useful with results
+            print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+
         return [[6, 0, 0, 0], [3, 0, 0, 0], [1, 0, 0, 0], [3, 5, 0, 0]]
