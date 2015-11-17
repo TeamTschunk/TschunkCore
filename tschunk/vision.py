4a1e90381e292ad9c1dff450e6ee7f899ce59611
Lukas Burgstaller <mail@voidstern.net>
2015-11-17 14:53:36 +0100

    Sample running in a direction

--- tschunk/vision.py
+++ tschunk/vision.py
@@ -8,4 +8,4 @@
 
 class vision(object):
     def get_commands_array (self):
-        return [[6, 0, 0, 0], [3, 0, 0, 0], [1, 0, 0, 0], [3, 5, 0, 0]]
+        return [[6, 3, 1, 3], [0, 0, 0, 5], [0, 0, 0, 0], [0, 0, 0, 0]]
