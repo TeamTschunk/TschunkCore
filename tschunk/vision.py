b51a60f90c0094434eaaebe253aa48ac24059bdb
PeterTheOne <petertheone@gmail.com>
2015-11-17 16:44:33 +0100

    crop captured image into slices and return commands_array

--- tschunk/vision.py
+++ tschunk/vision.py
@@ -7,30 +7,38 @@ class vision(object):
 
     cam = Camera(2)
 
-    def get_commands_array (self):
+    def get_commands_array(self):
         img = self.cam.getImage()
         img.save('image.jpg')
-
-        # create a reader
-        scanner = zbar.ImageScanner()
-
-        # configure the reader
-        scanner.parse_config('enable')
-
-        # obtain image data
         pil = img.getPIL().convert('L')
         width, height = pil.size
-        raw = pil.tostring()
 
-        # wrap image data
-        image = zbar.Image(width, height, 'Y800', raw)
+        scanner = zbar.ImageScanner()
+        scanner.parse_config('enable')
 
-        # scan the image for barcodes
-        scanner.scan(image)
+        image_count = 0
+        commands_array = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        x = 0
+        for left_pos in range(0, width, (width / 4)):
+            y = 0
+            for top_pos in range(0, height, (height / 4)):
+                image_count += 1
+                crop_box = (left_pos, top_pos, left_pos + (width / 4), top_pos + (height / 4))
+                crop_image = pil.crop(crop_box)
+                crop_image.save('image' + `image_count` + '.jpg')
 
-        # extract results
-        for symbol in image:
-            # do something useful with results
-            print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+                raw = crop_image.tostring()
+                # todo: what is Y800?
+                image = zbar.Image((width / 4), (height / 4), 'Y800', raw)
+                scanner.scan(image)
 
-        return [[6, 3, 1, 3], [0, 0, 0, 5], [0, 0, 0, 0], [0, 0, 0, 0]]
+                for symbol in image:
+                    # do something useful with results
+                    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+                    commands_array[x][y] = symbol.data
+                    break
+                y += 1
+            x += 1
+
+        print commands_array
+        return commands_array
